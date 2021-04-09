c3347e2ae78f38f3f477d256b61150169cc3ed94
Eugene M <darkstarmacho@gmail.com>
2021-04-09 11:47:28 +0300

    Display white screen with 5s delaying

--- src/main.c
+++ src/main.c
@@ -1,6 +1,34 @@
+//#include <SDL.h>
 #include <stdio.h>
+#include <SDL.h>
 
-int main() {
-    printf("Game running...");
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+int main(int argc, char* argv[]) {
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize SDL_Error: %s\n", SDL_GetError());
+    } else {
+        window = SDL_CreateWindow("SDL tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if(window == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        } else {
+            screenSurface = SDL_GetWindowSurface(window);
+
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xff, 0xff, 0xff));
+
+            SDL_UpdateWindowSurface(window);
+
+            SDL_Delay(5000);
+        }
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    
     return 0;
 }
